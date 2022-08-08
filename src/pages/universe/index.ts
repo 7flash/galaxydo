6ba1e19f638aae36d88785ce85e361c724f2aaa7
Berlian Gur <sevenflash@yandex.ru>
2022-08-08 14:35:05 +0800

    Delete index.ts

--- src/pages/universe/index.ts
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from "preact/hooks";
-import { useLocation } from "wouter-preact";
-import { useQuery, useMutation } from "@convex/_generated";
-
-export default function Universe() {
-
-}
